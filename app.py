46ab6b24e3ee285324dc30050bd510f5ca20fe0f
BanikGLT <thebjlactb@mail.ru>
2024-08-30 19:01:58 +0300

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
-from flask import Flask, request
+from quart import Quart, request
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Получаем токен бота из переменной окружения
-BOT_TOKEN = os.getenv("6630264932:AAFf9zYIgAlSTAp4AzCgikGKKXgWg44mIes")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     raise RuntimeError("Необходимо указать токен бота через переменную окружения BOT_TOKEN")
 
-# Инициализируем приложение Flask
-app = Flask(__name__)
+# Инициализируем приложение Quart
+app = Quart(__name__)
 
 # Создаем приложение Telegram с токеном
 telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
@@ -24,11 +24,12 @@ telegram_app.add_handler(CommandHandler("start", start))
 # Вебхук для обработки запросов от Telegram
 @app.route('/webhook', methods=['POST'])
 async def webhook():
-    update = Update.de_json(request.get_json(force=True), telegram_app.bot)
+    json_data = await request.get_json()
+    update = Update.de_json(json_data, telegram_app.bot)
     await telegram_app.process_update(update)
     return "ok"
 
-# Запуск Flask приложения
+# Запуск Quart приложения
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
