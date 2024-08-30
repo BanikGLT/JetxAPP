5aea57e4e76055f66ee6c1d91c42057d6020546a
BanikGLT <thebjlactb@mail.ru>
2024-08-30 03:26:40 +0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 app = Flask(__name__)
@@ -11,49 +11,33 @@ BOT_TOKEN = '6630264932:AAFf9zYIgAlSTAp4AzCgikGKKXgWg44mIes'
 # Инициализация Telegram Bot
 telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Хэндлеры для бота
+# Хэндлер для команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    website_url = "https://banikglt-jetxapp-d85a.twc1.net"  # URL вашего сайта
     keyboard = [
-        [InlineKeyboardButton("Лайт Режим", callback_data='lite')],
-        [InlineKeyboardButton("Рейдж Режим", callback_data='rage')]
+        [InlineKeyboardButton("Зайти в приложение", url=website_url)]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Выберите режим:', reply_markup=reply_markup)
+    await update.message.reply_text('Добро пожаловать! Нажмите кнопку ниже, чтобы зайти в приложение.', reply_markup=reply_markup)
 
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-    mode = query.data
-    coefficient = calculate_coefficient(mode)
-    await query.edit_message_text(text=f"Режим: {mode.capitalize()} - Коэффициент: {coefficient}")
-
-def calculate_coefficient(mode):
-    import random
-    if mode == 'lite':
-        return round(random.uniform(1.01, 2.2), 2)
-    elif mode == 'rage':
-        return round(random.uniform(1.7, 7), 2)
-
-# Роут для вебхука Telegram
+# Роут для Telegram вебхука
 @app.route('/webhook', methods=['POST'])
 async def webhook():
     update = Update.de_json(request.get_json(force=True), telegram_app.bot)
-    await telegram_app.update_queue.put(update)  # Добавили await здесь
+    await telegram_app.update_queue.put(update)
     return 'ok'
 
-# Основной рендеринг HTML страницы
+# Роут для отображения основного HTML интерфейса
 @app.route('/')
 def index():
     return render_template('predict.html')
 
-# Запуск Flask приложения
+# Главная функция запуска приложения
 def main():
     telegram_app.add_handler(CommandHandler('start', start))
-    telegram_app.add_handler(CallbackQueryHandler(button))
 
-    # Используем значение порта из окружения
-    port = int(os.getenv("PORT", 8080))  # Включает дефолтное значение для проверки
-    print(f"Application is running on port: {port}")  # Временный вывод для отладки
+    # Запуск Flask приложения
+    port = int(os.getenv("PORT", 8080))  # Порт будет установлен Timeweb Cloud
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
