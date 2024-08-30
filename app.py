7edb9d4797907978024a82ab51f642076aecc972
BanikGLT <thebjlactb@mail.ru>
2024-08-30 04:03:03 +0300

    app.py

--- app.py
+++ app.py
@@ -1,49 +1,50 @@
 from flask import Flask, request, render_template
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 import os
+import logging
+
+# Включаем логирование
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-# Токен вашего бота
-BOT_TOKEN = '6630264932:AAFf9zYIgAlSTAp4AzCgikGKKXgWg44mIes'
+# Используем переменную окружения для токена бота
+BOT_TOKEN = os.getenv('BOT_TOKEN')  # Убедитесь, что переменная окружения установлена
 
 # Инициализация Telegram Bot
-telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
+try:
+    telegram_app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Инициализация приложения
-async def initialize_app():
-    await telegram_app.initialize()
+    # Проверка успешного подключения
+    bot_info = telegram_app.bot.get_me()
+    logger.info(f"Bot successfully connected: {bot_info.username}")
+except Exception as e:
+    logger.error(f"Failed to connect bot: {e}")
+    raise
 
 # Хэндлер для команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    website_url = "https://banikglt-jetxapp-d85a.twc1.net"
     keyboard = [
-        [InlineKeyboardButton("Зайти в приложение", url=website_url)]
+        [InlineKeyboardButton("Зайти в приложение", url="https://banikglt-jetxapp-d85a.twc1.net")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Добро пожаловать! Нажмите кнопку ниже, чтобы зайти в приложение.', reply_markup=reply_markup)
+    await update.message.reply_text('Нажмите на кнопку ниже, чтобы зайти в приложение:', reply_markup=reply_markup)
 
 # Роут для Telegram вебхука
 @app.route('/webhook', methods=['POST'])
 async def webhook():
-    await initialize_app()  # Инициализация приложения
     update = Update.de_json(request.get_json(force=True), telegram_app.bot)
     await telegram_app.process_update(update)
     return 'ok'
-
-# Роут для отображения основного HTML интерфейса
-@app.route('/')
-def index():
-    return render_template('predict.html')
 
 # Главная функция запуска приложения
 def main():
     telegram_app.add_handler(CommandHandler('start', start))
 
     # Запуск Flask приложения
-    port = int(os.getenv("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)))
 
 if __name__ == '__main__':
     main()
